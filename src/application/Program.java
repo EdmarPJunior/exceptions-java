e2b2dae7ed94405b6666d40d4b226842719e94d9
Edmar Piacentini Junior <juniorpiacentini@hotmail.com>
2023-12-20 09:38:49 -0300

    Solucao ruim

--- src/application/Program.java
+++ src/application/Program.java
@@ -45,6 +45,7 @@ public class Program {
 			else {
 				reservation.updateDates(checkIn, checkOut);
 				System.out.println("Reservation: " + reservation);
+				System.out.println("fim");
 			}
 			
 			
